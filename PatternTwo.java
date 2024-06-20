aa1c023fd31de4bae83d749248ffc10660ca7cfd
Megha <chakrabortyreshmi077@gmail.com>
2024-06-20 21:48:50 +0530

    unknown pattern

--- PatternTwo.java
+++ PatternTwo.java
@@ -16,7 +16,7 @@ class PatternTwo{
             for(int i = 0; i <= n; i++){
            
             for(int j = n; j > i; j--){
-                System.out.print(" ");
+                System.out.print(/t);
             }
             for(int j = 0; j < i; j++){
                 System.out.print( num);
@@ -32,7 +32,7 @@ class PatternTwo{
            }
            
             for(int j = 0; j < i; j++){
-             System.out.print(" ");
+             System.out.print(/t);
             }
             System.out.println();
         }
